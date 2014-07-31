79c9c0b9e39e39865f5835e5c050329647478aec
ParamUppal <psuppal@yahoo.com>
2014-07-31 12:14:58 +0100

    PAPI POC Code

---
+++ ProductAPI/ProductAPI/Provider/Contract/IPriceProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductAPI.Entities.ProductEntities;
+namespace ProductAPI.Provider.Contract
+{
+    public interface IPriceProvider
+    {
+        PriceResult CalculateTotal(List<string> productIds,  string zone, DateTime? validFrom, DateTime? validTill, string applicablePromotionTypes);
+    }
+}
---
+++ ProductAPI/ProductAPI/Provider/GAPI/GapiPromotionProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProductAPI.Provider.Contract;
+using ProductAPI.Entities.ProductEntities;
+namespace ProductAPI.Provider.GAPI
+{
+    public class GapiPromotionProvider : BaseProvider, IPromotionProvider
+    {
+        
+       
+
+
+        public List<PromotionObject> Search(string q, string[] fields, int? limit, int? offset)
+        {
+            var uri = "http://172.25.58.107:67/v1/Promotion/search?q={0}&limit={1}&offset={2}&business=grocery";
+
+            var result = (List<PromotionObject>)FromApi("PromotionByProductIds", string.Empty, new string[] { q, limit.Value.ToString(), offset.Value.ToString() });
+
+            return result;
+        }
+    }
+}
+
