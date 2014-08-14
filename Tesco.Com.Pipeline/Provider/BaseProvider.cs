809da07ec12859586cd8d8f8774d7b8b8ae1852e
Afactor <avik_s1@yahoo.com>
2014-08-14 14:33:35 +0100

    capture error from api call

--- Tesco.Com.Pipeline/Provider/BaseProvider.cs
+++ Tesco.Com.Pipeline/Provider/BaseProvider.cs
@@ -13,58 +13,80 @@ using Tesco.Com.Pipeline.Utilities;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Configuration;
+using Tesco.Com.Pipeline.Utilities;
+
 namespace Tesco.Com.Pipeline.Provider
 {
     public class BaseProvider
     {
         protected object FromUri(string uri, HttpVerbs verb, string entityType, string entityAssembly, string body = "", bool isGenericType = false)
         {
-            //TODO:Cleaner code here
-            ObjectHandle handle = Activator.CreateInstance(entityAssembly, entityType);
-            var obj = handle.Unwrap();
-            Type t = obj.GetType();
-            //only list is supported
-            if (isGenericType)
-                obj = Utilities.HelperMethods.CreateGeneric(typeof(List<>), t);
-
-            Stream apiStream;
-
-            var httpClientHandler = new HttpClientHandler
+            try
             {
-                Proxy = ConfigurationManager.AppSettings["proxy"].Equals("default") || string.IsNullOrEmpty(ConfigurationManager.AppSettings["proxy"]) ?
-                WebRequest.DefaultWebProxy :
-                new WebProxy(ConfigurationManager.AppSettings["proxy"], false),                
-                UseProxy = true
-                
-            };
+                //TODO:Cleaner code here
+                ObjectHandle handle = Activator.CreateInstance(entityAssembly, entityType);
+                var obj = handle.Unwrap();
+                Type t = obj.GetType();
+                //only list is supported
+                if (isGenericType)
+                    obj = Utilities.HelperMethods.CreateGeneric(typeof(List<>), t);
 
-            //TODO: read up HttpCleint and refactor
+                Stream apiStream;
 
-            using (HttpClient client = new HttpClient(httpClientHandler,true))
-            {
-                client.DefaultRequestHeaders.Add("accept", "application/json");
-                client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json");
-                ////todo: remove hard code authetication. Use request headers
-                client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", "appKeyToken=DotnetHostUser001&appKey=F28AE227-529A-488D-A955-A0CD0048EC89&");
-                Logger.Info("Logging Request: uri: {0}. Body: {1}.");
-                
-
-                //TODO: Add delete
-                switch (verb)
+                var httpClientHandler = new HttpClientHandler
                 {
-                    case HttpVerbs.Post:
-                    case HttpVerbs.Put:
-                        apiStream = client.PostAsync(uri, new StringContent(body)).Result.Content.ReadAsStreamAsync().Result;
-                        break;
-                    default:
-                        apiStream = client.GetAsync(uri).Result.Content.ReadAsStreamAsync().Result;
-                        break;
+                    Proxy = ConfigurationManager.AppSettings["proxy"].Equals("default") || string.IsNullOrEmpty(ConfigurationManager.AppSettings["proxy"]) ?
+                    WebRequest.DefaultWebProxy :
+                    new WebProxy(ConfigurationManager.AppSettings["proxy"], false),
+                    UseProxy = true
+
+                };
+
+                //TODO: read up HttpCleint and refactor
+                HttpResponseMessage response = new HttpResponseMessage();
+                
+                using (HttpClient client = new HttpClient(httpClientHandler, true))
+                {
+                    client.DefaultRequestHeaders.Add("accept", "application/json");
+                    client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json");
+                    ////todo: remove hard code authetication. Use request headers
+                    client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", "appKeyToken=DotnetHostUser001&appKey=F28AE227-529A-488D-A955-A0CD0048EC89&");
+                    Logger.Info(string.Format("Logging Request: uri: {0}. Body: {1}.", uri, body));
+
+                    //TODO: Add delete
+                    switch (verb)
+                    {
+                        case HttpVerbs.Post:
+                        case HttpVerbs.Put:
+                            response = client.PostAsync(uri, new StringContent(body)).Result;
+                            break;
+                        default:
+                            response = client.GetAsync(uri).Result;
+                            break;
+                    }
+
                 }
-            }  
-            DataContractJsonSerializer serializer = new DataContractJsonSerializer(obj.GetType());
-            obj = serializer.ReadObject(apiStream);
-            apiStream.Close();
-            return obj;
+                if (response.IsSuccessStatusCode)
+                    apiStream = response.Content.ReadAsStreamAsync().Result;
+                else
+                {
+                    
+                    throw new ApiException(response.StatusCode, response.Headers);
+
+                }
+                
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(obj.GetType());
+                obj = serializer.ReadObject(apiStream);
+                response.Dispose();
+                apiStream.Close();
+                apiStream.Dispose();
+                return obj;
+            }
+            catch(ApiException apx)
+            {                
+                
+                throw;
+            }
         }
 
 
