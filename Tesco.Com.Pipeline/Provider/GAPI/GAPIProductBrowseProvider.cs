2a3cc5627edddd255f979b7f0fa2a1d5fc3c7e38
ParamUppal <psuppal@yahoo.com>
2014-09-10 13:29:44 +0100

    updated ProductBrowse Provider

--- Tesco.Com.Pipeline/Provider/GAPI/GAPIProductBrowseProvider.cs
+++ Tesco.Com.Pipeline/Provider/GAPI/GAPIProductBrowseProvider.cs
@@ -23,24 +23,20 @@ namespace Tesco.Com.Pipeline.Provider.GAPI
             if (productlist != null && productlist.ResultSubSet != null)
             {
                 StringBuilder q = new StringBuilder("productids=");
-                List<Lineitem> lineItems = new List<Lineitem>();
                 foreach (var prod in productlist.ResultSubSet)
                 {
                     q.Append(prod.ProductId).Append(",");
                     productIds.Add(prod.ProductId);
-                    lineItems.Add(new Lineitem() { ProductId = prod.ProductId.ToString()});
                 }
 
                 // remove last comma from the string
                 q.Remove(q.Length - 1, 1);
 
                 // Fetch Price for List of ProductIds make it Async
-                Arrayoflineitem arrLineitem = new Arrayoflineitem();
-                arrLineitem.LineItem = lineItems;
-                Products prods = new Products();
-                prods.ArrayOfLineItem = arrLineitem;
+                var body = productIds.Select(p => @"{""ProductId"":" + "\"" + p.ToString() + "\"" + "}").ToList();
+                var bodyText = string.Format("[{0}]", string.Join(",", body));
 
-                Entities.ProductPrice productPrice = (Entities.ProductPrice)FromApi("AnonymousProductBrowsePrice", JsonConvert.SerializeObject(prods),
+                Entities.ProductPrice productPrice = (Entities.ProductPrice)FromApi("AnonymousProductBrowsePrice", bodyText,
                     new string[] { DateTime.Today.ToString("yyyy-MM-dd"), DateTime.Today.ToString("yyyy-MM-dd"), "None", business });
 
                 // Fetch Product properties for a list of ProductIds, make it Async
